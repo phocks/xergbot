c21c61d515f7ddb9b0313ba30fea77a8e6560667
Joshua Byrd <phocks@gmail.com>
2022-11-22 15:39:13 +1000

    Test toot function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as dotenv from "dotenv";
 dotenv.config();
+import * as fs from "fs";
 
 import { to as wrap } from "await-to-js";
 
@@ -12,9 +13,26 @@ const id: string = process.env.MyId as string;
 async function main() {
   const [mastoErr, masto] = await wrap(getMasto());
 
-  await muteAll(masto, id);
+  // await unmuteAll(masto, id);
 }
 
 main().catch((error) => {
   console.error(error);
 });
+
+async function testToot(masto) {
+  // Upload the image
+  const attachment = await masto.mediaAttachments.create({
+    file: fs.createReadStream("src/img/test.png"),
+    description: "Some image",
+  });
+
+  // Toot!
+  const status = await masto.statuses.create({
+    status: "test toot from my toaster again...",
+    visibility: "public",
+    mediaIds: [attachment.id],
+  });
+
+  console.log(status);
+}
